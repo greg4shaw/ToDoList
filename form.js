4096437cd47fa4db468f8399344439fe2d6c93e1
greg <gman.international@gmail.com>
2021-05-27 17:39:48 -0400

    2021_05_27_v1

---
+++ form.js
@@ -0,0 +1,24 @@
+//Adding functionality with items specific to the form
+function ToDoForm({addTodo}) {
+    
+    const [value, setValue] = React.useState('');
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!value) return; 
+        addTodo(value);
+        setValue('');
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                className="input"
+                value={value}
+                placeholder="Add Todo ..."
+                onChange={e => setValue(e.target.value)}
+                />
+        </form>
+    );
+}
---
+++ todo.js
@@ -0,0 +1,9 @@
+function ToDo({todo, index, remove}) {
+
+    function handle(){
+        remove(index);
+    }
+    return(
+        <div className="todo" onClick={handle}>{todo.text} (-)</div>
+    );
+}
