8bcacf8dede498e9fda1546227f2b836619c6c1f
greg <gman.international@gmail.com>
2021-10-17 16:19:01 -0500

    2021_10_17

--- todo/src/App.test.js
+++ todo/src/App.test.js
@@ -1,31 +1,31 @@
 
-import React from 'react';
-import App from './App';
-import { fireEvent, render, wait } from '@testing-library/react'
-import userEvent from "@testing-library/user-event"
-import { api } from "./api"
-import { tsExternalModuleReference } from '@babel/types';
+// import React from 'react';
+// import App from './App';
+// import { fireEvent, render, wait } from '@testing-library/react'
+// import userEvent from "@testing-library/user-event"
+// import { api } from "./api"
+// import { tsExternalModuleReference } from '@babel/types';
 
-// TESTING PART 3 (Mocking)
+// // TESTING PART 3 (Mocking)
 
-// syntax needed to mock api
-const mockCreateItem = (api.createItem = jest.fn())
+// // syntax needed to mock api
+// const mockCreateItem = (api.createItem = jest.fn())
 
-test('add items to list', async () => {
-  const todoText = "learn spanish";
-  mockCreateItem.mockResolvedValueOnce(todoText);
-  const{getByText, getByPlaceholderText} = render(<App/>)
+// test('add items to list', async () => {
+//   const todoText = "learn spanish";
+//   mockCreateItem.mockResolvedValueOnce(todoText);
+//   const{getByText, getByPlaceholderText} = render(<App/>)
   
-  const input = getByPlaceholderText('Add ToDo ...');
-  userEvent.type(input, "wash car")
+//   const input = getByPlaceholderText('Add ToDo ...');
+//   userEvent.type(input, "wash car")
 
-  await wait(() => getByPlaceholderText('wash car'))
+//   await wait(() => getByPlaceholderText('wash car'))
 
-  expect(mockCreateItem).toBeCalledTimes(1);
-  expect(mockCreateItem).toBeCalledWith(
-  expect(input).toHaveValue("wash car")
-  )
-});
+//   expect(mockCreateItem).toBeCalledTimes(1);
+//   expect(mockCreateItem).toBeCalledWith(
+//   expect(input).toHaveValue("wash car")
+//   )
+// });
 
 // TESTING PART 2
 
@@ -69,32 +69,34 @@ test('add items to list', async () => {
 
 // TESTING PART 1
 
-// import React from 'react';
-// //import * as ReactDOM from 'react-dom'
-// import App from './App';
-// //import { getQueriesForElement } from '@testing-library/dom'
-// import { render } from '@testing-library/react'
+import React from 'react';
+//import * as ReactDOM from 'react-dom'
+import App from './App';
+//import { getQueriesForElement } from '@testing-library/dom'
+import { render } from '@testing-library/react'
 
-// // Create a div to be used to add our application into
-// // So this renders the app onto a virtual environment
+// Create a div to be used to add our application into
+// So this renders the app onto a virtual environment
 
-// test('ToDo', () => {
-//   // const root = document.createElement('div');
-//   // ReactDOM.render(<App/>, root)
+test('ToDo', () => {
+  // const root = document.createElement('div');
+  // ReactDOM.render(<App/>, root)
 
-//   // const {getByText, getByPlaceholderText} = getQueriesForElement(root)
+  // const {getByText, getByPlaceholderText} = getQueriesForElement(root)
 
-//   const{getByText, getByPlaceholderText} = render(<App/>)
+  const{getByText, getByPlaceholderText} = render(<App/>)
 
-//   // After rendering our component use DOM API's (query selector) to make assertions
-//   // select h1 and check if the text content is ToDo List
-// //   expect(root.querySelector('h1').textContent).toBe('ToDo List Never lose track of your tasks again...');
-// //   expect(root.querySelector('input').placeholder).toBe('Add ToDo ...');
-// // // This is no longer referring to the elements on the page
-// //   expect(getByText('ToDo List')).not.toBeNull();
-// //   expect(getByPlaceholderText('Add ToDo ...')).not.toBeNull()
-// // Better syntax
-//   getByText('ToDo List');
-//   getByPlaceholderText('Add ToDo ...');
+  // After rendering our component use DOM API's (query selector) to make assertions
+  // select h1 and check if the text content is ToDo List
+//   expect(root.querySelector('h1').textContent).toBe('ToDo List Never lose track of your tasks again...');
+//   expect(root.querySelector('input').placeholder).toBe('Add ToDo ...');
+// // This is no longer referring to the elements on the page
+//   expect(getByText('ToDo List')).not.toBeNull();
+//   expect(getByPlaceholderText('Add ToDo ...')).not.toBeNull()
+// Better syntax
+  getByText('ToDo List');
+  getByPlaceholderText('Add ToDo ...');
 
-// });
+});
+
+// "test": "react-scripts test"
